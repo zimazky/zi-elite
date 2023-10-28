64b487a9f231b558c82336a308dee0eca2809c4a
andrew zimatskiy <zimazky@gmail.com>
2023-10-29 02:02:22 +0300

    Выделены модули FBM volume noise

--- src/shaders/b/fs.glsl
+++ src/shaders/b/fs.glsl
@@ -57,7 +57,7 @@ layout (location = 2) out vec4 gAlbedo;
 // Модуль определения функций расчета пересечения луча с поверхностью
 // ----------------------------------------------------------------------------
 #ifndef RAYCAST_MODULE
-#include "src/shaders/common/raycasting/Raycast.glsl";
+#include "src/shaders/common/Raycasting/Raycast.glsl";
 #endif
 
 // ----------------------------------------------------------------------------
@@ -78,110 +78,22 @@ layout (location = 2) out vec4 gAlbedo;
 // Формирование G-буфера
 // ----------------------------------------------------------------------------
 
-/** 
- * Рейкастинг для случая плоской поверхности планеты
- *   ro - положение камеры
- *   rd - направление луча из камеры
- *   tmin - начальное глубина рейтрейсинга
- *   tmax - максимальная глубина рейтрейсинга
- *   i - выходное значение количества циклов рейтрейсинга
- */
- /*
-float raycast(vec3 ro, vec3 rd, float tmin, float tmax, out int i) {
-  float t = tmin;
-  float d = ro.y - MAX_TRN_ELEVATION;
-  if(d >= 0.) t = clamp(-d/rd.y, t, tmax); // поиск стартовой точки, если камера выше поверхности максимальной высоты гор
-
-  for(i=0; i<300; i++) {
-    vec3 pos = ro + t*rd;
-    if(pos.y>ro.y && pos.y>MAX_TRN_ELEVATION) return tmax + 1.;
-    float h = pos.y - terrainM(pos.xz);
-    if( abs(h)<(0.003*t) || t>tmax ) break; // двоятся детали при большем значении
-    t += 0.4*h; // на тонких краях могут быть артефакты при большом коэффициенте
-  }
-  return t;
-}
-*/
-
-bool isIntersectMaxTerrain(vec3 ro, vec3 rd, out float dist) {
-  float alt = lonLatAlt(ro).z;
-  dist = 0.;
-  if(alt < MAX_TRN_ELEVATION) return true;
-  vec3 r = uPlanetCenter - ro;
-  float OT = dot(rd, r);
-  if(OT < 0.) return false; // луч уходит от планеты
-  float CO = length(r);
-  float CT2 = CO*CO - OT*OT;
-  float R = uPlanetRadius + MAX_TRN_ELEVATION;
-  float R2 = R*R;
-  if(CT2 >= R2) return false; // луч не пересекается со сферой
-  float AT = sqrt(R2 - CT2);
-  dist = OT-AT;
-  return true;
-}
-
-/** 
- * Рейкастинг для случая сферической поверхности планеты 
- *   ro - положение камеры
- *   rd - направление луча из камеры
- *   tmin - начальное глубина рейтрейсинга
- *   tmax - максимальная глубина рейтрейсинга
- *   i - выходное значение количества циклов рейтрейсинга
- */
-float raycastSpheric(vec3 ro, vec3 rd, float tmin, float tmax, out int i) {
-  float t = tmin;
-  float altPrev = lonLatAlt(ro).z;
-
-  if(altPrev > MAX_TRN_ELEVATION) {
-    vec3 r = uPlanetCenter - ro;
-    float OT = dot(rd, r);
-    if(OT < 0.) return 1.01 * MAX_TERRAIN_DISTANCE; // луч уходит от планеты
-    float CO = length(r);
-    float CT2 = CO*CO - OT*OT;
-    float R = uPlanetRadius + MAX_TRN_ELEVATION;
-    float R2 = R*R;
-    if(CT2 >= R2) return 1.01 * MAX_TERRAIN_DISTANCE; // луч не пересекается со сферой
-    float AT = sqrt(R2 - CT2);
-    t = max(tmin, OT-AT);
-    tmax += t;
-  }
-  
-  for(i=0; i<600; i++) {
-    vec3 pos = ro + t*rd;
-    float alt = lonLatAlt(pos).z;
-    if(alt>altPrev && alt>=MAX_TRN_ELEVATION) return 1.01 * MAX_TERRAIN_DISTANCE;
-    altPrev = alt;
-    float h = alt - terrainHeight(pos, t);
-    if( abs(h)<(0.003*t) ) return t; // двоятся детали при большем значении
-    t += 0.5*h; // на тонких краях могут быть артефакты при большом коэффициенте
-    if(t>tmax) return 1.01 * MAX_TERRAIN_DISTANCE;
-  }
-  return t;
-}
 
 void main(void) {
   vec2 uv = (gl_FragCoord.xy - 0.5*uResolution.xy)/uResolution.x;
   vec3 rd = normalize(vRay);
-/*
-  if(uScreenMode.x == MAP_VIEW) {
-    // Режим отображения карты
-    vec4 norDepth = vec4(0);
-    //gAlbedo = vec4(showMap(uCameraPosition, uCameraDirection.xz, uv, int(uScreenMode.y), norDepth), 1);
-    gNormalDepth = norDepth;
-    return;
-  }
-*/
+
   #ifdef DEPTH_ERROR_VIEW
   // Режим для просмотра ошибки глубины между расчетным значением и предсказанием на основе предыдущего кадра
   float t0 = 1.;
-  #else
+  #else //DEPTH_ERROR_VIEW
   // Нормальный режим, с испльзованием данных предыдущего кадра
   float t0 = texture(uTextureADepth, gl_FragCoord.xy/uResolution).r;
-  #endif
+  #endif //DEPTH_ERROR_VIEW
 
   vec3 col = vec3(0);
   int raycastIterations = 0;
-  float LvsR = step(0.5, gl_FragCoord.x/uResolution.x);
+  float LvsR = step(0.8, gl_FragCoord.x/uResolution.x);
   if(t0 >= MAX_TERRAIN_DISTANCE) {
     gNormal = -rd;
     gDepth = 1.01 * MAX_TERRAIN_DISTANCE;
@@ -198,7 +110,7 @@ void main(void) {
     else {
       vec3 pos = uCameraPosition + nor_t.w*rd;
       vec3 nor = nor_t.xyz;
-      //vec3 nor = mix(nor_t.xyz, terrainNormal(pos).xyz, LvsR);
+      if(LvsR == 1.) nor = terrainNormal(pos, nor_t.w).xyz;
       gNormal = nor;
       gDepth = nor_t.w;
       //gNormalDepth = nor_t;//vec4(nor, t);
---
+++ src/shaders/common/terrain/FlatFBM.glsl
@@ -0,0 +1,75 @@
+#define TERR_MODULE
+// ----------------------------------------------------------------------------
+// Генерация ландшафта - InigoQuilezFBM на плоскости XZ
+// ----------------------------------------------------------------------------
+
+#define TERR_FLAT // Определение ландшафта на плоскости
+
+
+
+// Радиус планеты
+uniform float uPlanetRadius;
+// Положение центра планеты
+uniform vec3 uPlanetCenter;
+
+const float W_SCALE = 3000.; // масштаб по горизонтали
+const float H_SCALE = 1100.; // масштаб по высоте
+const float MAX_TRN_ELEVATION = 1.9*H_SCALE; // максимальная высота
+
+// ----------------------------------------------------------------------------
+// Модуль расчета фрактального шума
+// ----------------------------------------------------------------------------
+
+#ifndef FBMNOISE_MODULE
+#include "src/shaders/common/Noise/FbmRidged2.glsl";
+//include "src/shaders/common/Noise/FbmInigoQuilez.glsl";
+#endif
+
+// Перевод декартовых координат точки в псевдосферические координаты для плоской поверхности
+// Начало декартовых координат совпадает с точкой 0,0,0
+// Возвращается:
+// x - долгота (координата x)
+// y - широта (координата z)
+// z - высота над поверхностью (координата y)
+vec3 lonLatAlt(vec3 p) { return p.xzy; }
+
+// Единичный вектор направленный в зенит
+vec3 terrainZenith(vec3 p) { return vec3(0, 1, 0); }
+
+// Вектор относительно центра планеты
+vec3 terrainFromCenter(vec3 p) { return vec3(0, p.y + uPlanetRadius, 0); }
+
+const float nScale = H_SCALE/W_SCALE;
+// pos - координаты точки
+// dist - дистанция от камеры до точки
+// uv - выходное значение текстурных кординат
+// возвращает:
+// xyz - нормаль
+// w - высота
+vec4 height_d(vec3 pos, float dist, out vec2 uv) {
+  uv = pos.xz;
+  vec4 h_d = terrainFbm(uv/W_SCALE, dist);
+  h_d.z /= nScale;
+  return vec4(normalize(h_d.xzy), H_SCALE*h_d.w);
+}
+
+float terrainHeight(vec3 pos, float dist) {
+  vec2 uv;
+  vec4 h_d = height_d(pos, dist, uv);
+  return h_d.w;
+}
+
+vec4 terrainHeightNormal(vec3 pos, float dist, out vec2 uv) {
+  vec4 h_d = height_d(pos, dist, uv);
+  return h_d;
+}
+
+// Вычисление нормали под точкой
+vec3 terrainNormal(vec3 pos, float dist) {
+  vec2 eps = vec2(0.01, 0.);
+  return normalize(vec3(
+    terrainHeight(pos - eps.xyy, dist) - terrainHeight(pos + eps.xyy, dist),
+    2.*eps.x,
+    terrainHeight(pos - eps.yyx, dist) - terrainHeight(pos + eps.yyx, dist)
+  ));
+}
